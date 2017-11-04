d7d7d010a1aeb90605f3fcb366e95e1f55be50ec
Vincent Ngeno <khalifngeno@gmail.com>
2017-11-04 18:56:40 +0300

    add s functions to append the scores of various playes as obtained from business logic to section three

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,15 @@ function Players(name,turnTotal,totalScore){
 	this.totalScore=totalScore;
 }
 
+function randomNumbers (){
+	math.rand();
+}
+
 function results(randnum){
 	if (randnum===1){
 		return turnTotal=0;
          }
-    else if{
+    else {
     	return turnTotal+=randnum;
     }
 }
@@ -29,10 +33,27 @@ $(document).ready(function(){
 
 	$("form#form-group").submit(function(event){
 		event.preventDefault();
-		var name=$("player1").val();
-		var player1=new Players(name)
+		console.log("ok");
+		var name1=$("#player1").val();
+		var name2=$("#player2").val();
+		var player1=new Players(name1)
+		var player2=new Players(name2)
+		console.log("ok");
 
-	$("#player1-append").append(player1.name)
+	$("h3#player1-append").append(player1.name)
+	$("h3#player2-append").append(player2.name)
+	console.log("ok");
+	});
+
+	$("button#play-green").click(function(event){
+		event.preventDefault();
+		alert (randomNumbers());
+	});
+
+	$("button#play-red").click(function(event){
+		event.preventDefault();
+
+		$("#rollScore2").text(turnTotal);
 	});
 
 
