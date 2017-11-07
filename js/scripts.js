9b14dcf7e6fa3ac522f98be30887b21c8406db7e
Vincent Ngeno <khalifngeno@gmail.com>
2017-11-07 08:03:10 +0300

    git add animation effect to the submit button targeting other elements on the pages

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,17 @@ function Players(playerName,turnTotal,totalScoreValue){
 	this.turnTotal=turnTotal;
 	this.totalScoreValue=totalScoreValue;
 }
+var randnum=Math.floor(Math.random()*6)+1;
 
-Players.prototype.randomNumbers= function(){
-	var randnum=Math.floor(Math.random()*6)+1;
+
+Players.prototype.randomNumi= function(){
 	if (randnum===1){
 		this.turnTotal=0;
 	}
 	else{
 		this.turnTotal=this.turnTotal+randnum;
-	}
+	};
+	return this.turnTotal;
 }
 
 // this section function will help in calculating the score
@@ -33,30 +35,43 @@ $(document).ready(function(){
 		var nameOfPlayer2= $("#player2").val();
 		console.log(nameOfPlayer1);
 
-		var player1= new Players(nameOfPlayer1,0,0);
-		var player2= new Players(nameOfPlayer2,0,0);
+		player1= new Players(nameOfPlayer1,0,0);
+		player2= new Players(nameOfPlayer2,0,0);
 		console.log("ok");
-         
-        console.log(player1.playerName);
+
 		$("#player1-append").text(nameOfPlayer1);
 		$("#player2-append").text(nameOfPlayer2);
 		console.log("ok");
 
-		$("#play-green-1").click(function(event){
-			event.preventDefault();
-			console.log("ok");
-			debugger
-
-			var turnOutput1= player1.randomNumbers();
-			if (turnOutput1===1){
-				$("#rollScore1-value").text(turnOutput1);
-				$("#totalScore1-value").text(player1.turnScore);
-			}
+		$("#rules").slideToggle();
+		$("#player1").slideToggle();
+		$("#player2").slideToggle();
+		$("#top").slideToggle();
 
 
-		});
-		
 	});
+
+		$("#play-green-1").click(function(event){
+		event.preventDefault();
+		console.log("ok");
+
+		debugger;
+	
+	    $("#totalScore1-value").text("ok");
+	    $("#rollScore1-value").text("ok");
+
+		var turnOutput1= player1.randomNumi();
+		console.log(turnOutput1);
+		if (turnOutput1===1){
+			$("#rollScore1-value").text(turnOutput1);
+			$("#totalScore1-value").text(player1.turnScore);
+		}else{
+			$("#totalScore1-value").text(player1.totalScore);
+		}
+
+
+	});
+		
 
 });
 
